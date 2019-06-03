3ef2772433ac87812dda94b35abe2a2bf84ea030
Fuji Nguyen <fuji.nguyen@live.com>
2019-06-03 09:45:39 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,7 +13,7 @@ import env from './.env';
 export const environment = {
   production: false,
   version: env.npm_package_version + '-dev',
-  serverUrl: '/api',
+  serverUrl: 'https://api.chucknorris.io',
   defaultLanguage: 'en-US',
   supportedLanguages: [
     'en-US',

f5234e4ba5df03a265e1deebe49d842b990c34ee
fuji.nguyen <fuji.nguyen@live.com>
2019-06-05 21:41:16 -0400

    mock data update

--- src/app/catalog/catalogitem.mockdata.ts
+++ src/app/catalog/catalogitem.mockdata.ts
@@ -5,77 +5,77 @@ export const CatalogItems: CatalogItem[] = [
     id: 1,
     name: 'ACRS',
     icon: 'f/f3/Flag_of_Russia.svg',
-    description: 'Automated Classification and Recruitment Solution',
+    description: 'Automic Classification and Recreation Solution',
     category: 'Position Classification, Recruiting, Hiring'
   },
   {
     id: 2,
     name: 'ERP',
     icon: 'c/c3/Flag_of_France.svg',
-    description: 'Employee Retirement Portal',
+    description: 'Extraterrestrial Retirement Portal',
     category: 'Benefits, Retirement'
   },
   {
     id: 3,
     name: 'GADTRK',
     icon: 'b/ba/Flag_of_Germany.svg',
-    description: 'Grievance Appeals and Disciplinary Actions Tracking System',
+    description: 'Grand Appeals and Decision Actions Tracking System',
     category: 'Employee Labor Relations, Reasonable Accommodations'
   },
   {
     id: 4,
     name: 'BATS',
     icon: '5/5c/Flag_of_Portugal.svg',
-    description: 'Budget Allocations Tracking System',
+    description: 'Build Allocations Tracking System',
     category: 'Enterprise Support'
   },
   {
     id: 5,
     name: 'BMA',
     icon: 'c/cf/Flag_of_Canada.svg',
-    description: 'BMA',
+    description: 'Bomb Management Assembly',
     category: 'Performance Management'
   },
   {
     id: 6,
     name: 'GEMS',
     icon: '2/21/Flag_of_Vietnam.svg',
-    description: 'GEMS',
+    description: 'Goldern Enterprise Management System',
     category: 'Personnel Actions'
   },
   {
     id: 7,
     name: 'PCS TV',
     icon: '0/05/Flag_of_Brazil.svg',
-    description: 'PCS Travel Claim System',
+    description: 'Personnel Control Space Travel Vehicle',
     category: 'Travel'
   },
   {
     id: 8,
     name: 'EAPS',
     icon: 'f/fc/Flag_of_Mexico.svg',
-    description: 'Executive Agency Personnel Support',
+    description: 'Extreme Advertisement Program Support',
     category: 'Workforce Planning, Analytics'
   },
   {
     id: 9,
     name: 'REETA',
     icon: 'a/a4/Flag_of_the_United_States.svg',
-    description: 'Recruitment Examination Evaluation Tracking Application',
+    description: 'Retention Examination Evacuation Tracking Application',
     category: 'Position Classification, Recruiting, Hiring'
   },
   {
     id: 10,
     name: 'MCLASS',
     icon: '4/41/Flag_of_India.svg',
-    description: 'Mission Classification',
+    description: 'Missle Classification',
     category: 'Position Classification, Recruiting, Hiring'
   },
   {
     id: 11,
     name: 'SLRP',
     icon: '9/9f/Flag_of_Indonesia.svg',
-    description: 'Student Loan Repayment Program',
+    description: 'Space Loan Repayment Program',
     category: 'Benefits, Retirement'
   },
   {
@@ -89,7 +89,7 @@ export const CatalogItems: CatalogItem[] = [
     id: 13,
     name: 'EP',
     icon: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
-    description: 'Employee Profile',
+    description: 'Employee Process',
     category: 'Personnel Actions'
   }
 ];
